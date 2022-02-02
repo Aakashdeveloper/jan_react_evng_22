51d521cdaddc60c6c9608e312cbe1b7ea1bd6dfc
aakashdeveloper <ahanda205@gmail.com>
2022-02-02 19:22:51 +0530

    firstappend

--- firstapp/src/component/Home.js
+++ firstapp/src/component/Home.js
@@ -9,19 +9,26 @@ class Home extends Component {
         super()
 
         this.state={
-            productData:JSON
+            productData:JSON,
+            filteredData:JSON
         }
     }
 
+    /*var a = [2,3,7,6,8,9,12]
+        a.filter((data) => {return data>5})*/
+
     filterProduct = (keyword) => {
-        console.log("in home>>> ", keyword)
+        var output = this.state.productData.filter((item) => {
+            return (item.name.toLowerCase().indexOf(keyword.toLowerCase())>-1) 
+        })
+        this.setState({filteredData:output})
     }
 
     render(){
         return (
             <>
                 <Header userText={(data) => {this.filterProduct(data)}}/>
-                <Product prodData={this.state.productData}/>
+                <Product prodData={this.state.filteredData}/>
                 <Footer year="2022"/>
             </>
         )
